c761b35a7daf9cc384d5020ee96a41be9aff3d30
Junoh Moon <snowphone@Laboratory-PC.localdomain>
2018-11-24 22:25:17 +0900

    밤에 켜지지 않음, 냉/난방시 최저/최고온도로 자동 설정

--- AirConditioner.py
+++ AirConditioner.py
@@ -37,15 +37,40 @@ class Browser:
 
 		self.browser = browser
 		self.child = None
+		self.id = None
+		self.pw = None
 	
-	def turn_on_once(self, identification, password):
+	def turn_up(self):
+		increase_button=self.browser.find_element_by_id("OverImage_2")
+		while True:
+			try:
+				increase_button.click()
+			except UnexpectedAlertPresentException:
+				self.browser.switch_to.alert.accept()
+				return
+
+	def turn_down(self):
+		reduce_button=self.browser.find_element_by_id("OverImage_3")
+		while True:
+			try:
+				reduce_button.click()
+			except UnexpectedAlertPresentException:
+				return
+
+	def login(self):
 		url = "http://203.249.68.52"
 		self.browser.get(url)
-		self.browser.find_element_by_id("txtId").send_keys(identification)
+		self.browser.find_element_by_id("txtId").send_keys(self.id)
 
-		self.browser.find_element_by_id("txtPwd").send_keys(password)
+		self.browser.find_element_by_id("txtPwd").send_keys(self.pw)
 		self.browser.find_element_by_id("btnLogin").click()
+		return
 
+	def turn_on_once(self, identification, password):
+		self.id = identification
+		self.pw = password
+
+		self.login()
 
 		try:
 			img_src=self.browser.find_element_by_id("Image_OnOff").get_attribute("src")
@@ -58,14 +83,18 @@ class Browser:
 		coldWarmCheck=self.browser.find_element_by_id("Image_1").get_attribute("src")
 		if coldWarmCheck.find("nn_0") != -1:
 			root.title("web remote - 난방")
+			#increase temperature as warm as possible
+			self.turn_up()
 		else:
 			root.title("web remote - 냉방")
+			#reduce temperature as warm as possible
+			self.turn_down()
 
 		turnOnSucceeded = False
 		if img_src.find("this_1") == -1: 
 			self.browser.find_element_by_id("OverImage_OnOff").click()
 			turnOnSucceeded = True
-			print("turning on the AC.", end=' ', flush=True)
+			print("The AC has just turned on.", end=' ', flush=True)
 		else:
 			print("The AC is still running.", end=' ', flush=True)
 		self.browser.find_element_by_id("btnSubmit").click()
@@ -74,12 +103,8 @@ class Browser:
 		self.browser.find_element_by_id("btnLogout").click()
 		return turnOnSucceeded
 	
-	def turn_off(self, identification, password):
-		self.browser.get("http://203.249.68.52")
-		self.browser.find_element_by_id("txtId").send_keys(identification)
-
-		self.browser.find_element_by_id("txtPwd").send_keys(password)
-		self.browser.find_element_by_id("btnLogin").click()
+	def turn_off(self):
+		self.login()
 
 		img_src=self.browser.find_element_by_id("Image_OnOff").get_attribute("src")
 
@@ -135,14 +160,18 @@ pwTextBox.grid(row=1, column=1, padx=MARGIN, pady=MARGIN)
 
 
 def turn_on_closure(): 
-	ret = browser.turn_on_once(idText.get(), pwText.get())
-	if ret is None:
-		return
-	elif ret:
+	if not isDaytime():
+		print("It's night.", end=' ')
 		sleeptimerInMin = 120
 	else:
-		sleeptimerInMin = 3
-	print("sleep for {} minutes".format(sleeptimerInMin))
+		ret = browser.turn_on_once(idText.get(), pwText.get())
+		if ret is None:
+			return
+		elif ret:
+			sleeptimerInMin = 120
+		else:
+			sleeptimerInMin = 3
+		print("It'll check up in {} minutes".format(sleeptimerInMin))
 	root.after(int(sleeptimerInMin * 60 * 1000), turn_on_closure)
 
 startButton = Button(leftFrame, text="start", command=turn_on_closure)
@@ -150,7 +179,7 @@ startButton.grid(columnspan=2, padx=MARGIN, pady=MARGIN)
 pwTextBox.bind("<Return>", lambda event: turn_on_closure())
 startButton.bind("<Return>", lambda event: turn_on_closure())
 
-log = Text(root, width=50, height=10)
+log = Text(root, width=60, height=10)
 log.grid(row=0, column=2, padx=MARGIN, pady=MARGIN)
 sys.stdout = Redirector(log)
 
@@ -162,7 +191,7 @@ if len(sys.argv) == 3:
 
 root.mainloop()
 try:
-	browser.turn_off(idText.get(), pwText.get())
+	browser.turn_off()
 except:
 	pass
 finally:
